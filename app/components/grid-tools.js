66257423b378f202477dbc8d19d69d21ab761944
Marcel Ueno <japones@trampos.co>
2015-09-20 14:41:18 -0300

    grid tools

--- app/components/grid-tools.js
+++ app/components/grid-tools.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import Draggable from 'npm:gsap/src/uncompressed/utils/Draggable';
 
 export default Ember.Component.extend({
+	classNames:['grid-tools'],
+	
+	setup: function(){
+		console.log(this.$().parent())
+		Draggable.create(this.$(), { type:"x,y", bounds:this.$().parent(), throwProps:true});
+	}.on('didInsertElement')
 });
